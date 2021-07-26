d0866bca3ffa94e8aecc7f5d3a27d115a9781978
tboccuto <59341700+tboccuto@users.noreply.github.com>
2021-07-26 15:57:56 -0400

    adding premade bootstrap for some style

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :posts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #make landing page the posts page
+  root 'posts#index'
 end
